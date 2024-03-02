d194f417b6eaef5e31bf103c9927c01edcedd577
henry <108975468+Henry-Edet@users.noreply.github.com>
2024-03-02 18:15:48 +0200

    new update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     plugins : {
-        tailwindcss: {},
-        autoprefixer: {},
+        plugins: [require("taiwindcss"), require("autoprefixer")],
     }
 }
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <Components.Hero />
+      <Components.Header />
     </div>
   );
 }
