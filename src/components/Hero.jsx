fddefc28ba6ec483a616278a768992f74af85889
Famak2003 <Famaki310@gmail.com>
2024-03-02 20:43:32 +0300

    learn code completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,43 @@
+import HEROIMG from "./../assets/Hero-img.png";
+
+const Button = ({ bgClr, textClr, children }) => {
+  return (
+    <button
+      style={{ background: bgClr, color: textClr }}
+      className=" text-[1.5rem] text-[white] ring-1 px-[1.5rem] py-[.7rem] rounded-full "
+    >
+      {children}
+    </button>
+  );
+};
+
 function Hero() {
   return (
-    <section className=" text-center">
-      <h1 className=" generalPadding  ">
+    <section className=" text-center flex flex-col gap-[2rem] generalPadding">
+      <h1 className=" generalPadding ">
         Learn, code, connect. Become a Code Crafter.
       </h1>
-      <p className=" text-[1.5rem]">
-        Unlock your potential, unlock the future. Code with us.
-      </p>
-      <div className=" flex gap-[3rem] w-full ">
-        <button className=" text-[1.5rem] ring-1 px-[1rem] py-[.5rem] rounded-3xl bg-black">
-          Signup
-        </button>
-        <button>Contact Us</button>
+      <div className=" flex flex-col gap-[.5rem]">
+        <p className=" text-[1.5rem]">
+          Unlock your potential, unlock the future. Code with us.
+        </p>
+
+        <div className=" flex justify-center items-center gap-[3rem] w-full ">
+          <Button bgClr={"var(--black"} textClr={"white"}>
+            Signup
+          </Button>
+          <Button bgClr={"var(--blue)"} textClr={"white"}>
+            Contact Us
+          </Button>
+        </div>
       </div>
+      <figure className=" h-[24.5rem] overflow-hidden rounded-3xl ">
+        <img
+          className=" object-cover h-full w-full"
+          src={HEROIMG}
+          alt="hero-img"
+        />
+      </figure>
     </section>
   );
 }
