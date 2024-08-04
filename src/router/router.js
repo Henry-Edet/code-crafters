e893f96eec79dc7f7add4c5eb4999f0258dc55a1
henry edet <ghost@henrys-MacBook-Air.local>
2024-08-04 18:29:13 +0300

    added signup to router

--- src/router/router.js
+++ src/router/router.js
@@ -1,20 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import AppRoute from "./AppRoute";
+import { Signup } from "../pages/auth/auth";
 
-const router = createBrowserRouter(
-    [
-        {
-            element: <AppRoute/>,
-            children: [
-                {
-                    path: "/",
-                    element: <Home/>
-
-                }
-            ]
-               
-        }
-    ]
-)
-export default router
+const router = createBrowserRouter([
+  {
+    element: <AppRoute />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/auth",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+export default router;
